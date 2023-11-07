ea80df33330aaee8d6f01bb0b0b3764c7d59e28d
ZeFurtado <lukhasfurtado9@gmail.com>
2023-11-07 07:19:05 -0300

    Removi os .class criados pelo Blue J. A criação de lojas pela classe principal está funcionando.

--- Desafio Lab1/Etapa 2/src/Shopping/Endereco.java
+++ Desafio Lab1/Etapa 2/src/Shopping/Endereco.java
@@ -11,12 +11,22 @@ public class Endereco {
     private String complemento;
 
 
+    public Endereco(String nomeDaRua, String cidade, String estado, String pais, String cep, String numero){
+        this.nomeDaRua = nomeDaRua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+        this.numero = numero;
+    }
+
     public Endereco(String nomeDaRua, String cidade, String estado, String pais, String cep, String numero,String complemento){
         this.nomeDaRua = nomeDaRua;
         this.cidade = cidade;
         this.estado = estado;
         this.pais = pais;
         this.cep = cep;
+        this.numero = numero;
         this.complemento = complemento;
     }
 
--- Desafio Lab1/Etapa 2/src/Shopping/Principal.java
+++ Desafio Lab1/Etapa 2/src/Shopping/Principal.java
@@ -3,30 +3,70 @@ package Shopping;
 public class Principal {
     public static void main(String[] args){
         int opcao;
-        Endereco enderecoLoja1 = new Endereco("Rua dos Bobos", "Piraporinha", "SC", "Uruguai", "99000-100", "0", "Fundos");
-        Data dataFundacaoLoja1 = new Data(10, 3, 2023);
 
         StringBuilder opcoes = new StringBuilder();
         opcoes.append("\nDigite uma opção: \n");
         opcoes.append("(1) Criar uma loja \n");
         opcoes.append("(2) Criar um produto \n");
         opcoes.append("(3) Sair");
+
         do{
-            opcao = Teclado.leInt(opcoes.toString());
+            System.out.println(opcoes);
+            opcao = Teclado.leInt();
 
             if(opcao == 1 || opcao == 2 || opcao == 3){
                 switch (opcao){
 
-                    case '1':
-                        Loja lojaTeste = new Loja("Loja1", 5, 3000.00, enderecoLoja1, dataFundacaoLoja1);
+                    case 1:
+                        //Valores para o construtor da Loja
+                        System.out.println("Digite o nome da Loja:");
+                        String nomeDaLoja = Teclado.leString();
+                        System.out.println("Digite a quantidade de funcionários:");
+                        int quantidadeDeFuncionarios = Teclado.leInt();
+                        System.out.println("Digite o salário base dos funcionários:");
+                        double salarioBaseFuncionario = Teclado.leDouble();
+
+                        //Valores do construtor do Endereço
+                        System.out.println("Digite o nome da rua da loja:");
+                        String nomeDaRua = Teclado.leString();
+                        System.out.println("Digite a cidade:");
+                        String cidade = Teclado.leString();
+                        System.out.println("Digite as iniciais do estado (Ex. RS, MG, SP): ");
+                        String estado = Teclado.leString();
+                        System.out.println("Digite o nome do país:");
+                        String pais = Teclado.leString();
+                        System.out.println("Digite o número do CEP:");
+                        String cep = Teclado.leString();
+                        System.out.println("Digite o número do local:");
+                        String numero = Teclado.leString();
+                        System.out.println("Digite algum complemento");
+                        String complemento = Teclado.leString();
+
+                        //Valores para a criação da data
+                        System.out.println("Digite o dia da fundação da Loja:");
+                        int dia = Teclado.leInt();
+                        System.out.println("Digite o mês da fundação da Loja");
+                        int mes = Teclado.leInt();
+                        System.out.println("Digite o ano da funfação da Loja (Ex. 1995): ");
+                        int ano = Teclado.leInt();
+
+                        //Agora é chamado os contrutes de cada classe (Data, Endereço e Loja) para criar o objeto da Loja
+                        Data data = new Data(dia, mes, ano);
+                        Endereco endereco = new Endereco(nomeDaRua, cidade, estado, pais, cep, numero, complemento);
+                        Loja loja = new Loja(nomeDaLoja, quantidadeDeFuncionarios, salarioBaseFuncionario, endereco, data);
+
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("Foi criada a loja: ");
+                        System.out.println(loja.toString());
                         break;
 
-                    case '2':
+                    case 2:
 
 
                         break;
 
-                    case '3':
+                    case 3:
                         System.out.println("Bye! Bye!");
                         break;
 
@@ -37,8 +77,6 @@ public class Principal {
 
 
         }while(opcao != 3);
-
-
 
     }
 
